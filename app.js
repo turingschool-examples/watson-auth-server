595952fe69e63769ee76524fb41907bd69a8803a
Khalid Williams <khalidwill27@gmail.com>
2020-07-30 16:02:54 -0600

    Edit the route names and add set up for post

--- app.js
+++ app.js
@@ -9,24 +9,39 @@ const port = process.env.PORT || 5000;
 const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 cosnt { IamAuthenticator } = require('ibm-watson/auth');
 
-const getAuthentication = () => {
+const getAuthenticator = () => {
   const authenticator = new IamAuthenticator({
     apiKey: process.env.API_KEY
   })
   return authenticator;
 }
 
+const toneAnalyzer = new ToneAnalyzerV3({
+  version: '{version}',
+  authenticator: getAuthenticator,
+  url: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com',
+  disableSslVerification: true,
+});
+
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/authentication', (req, res) => { 
+app.get('/api/authenticator`', (req, res) => { 
   //
   // I'm not sure how sending the auth as json will work, but this would be for
   // if you just wanted to get your auth token from the server and do everything 
   // else on the client side
   //
-  const auth = getAuthentication;
-  res.status(200).json({authentication: JSON.stringify(auth)}) 
+  const auth = getAuthenticator;
+  res.status(200).json({authenticator: JSON.stringify(auth)}) 
+});
+
+app.post('/api/tone', (req, res) => {
+  // This route should get a bodyt with some text 
+  // and make a request to the tone analysis api endpoint 
+  // via the toneAnalyser above 
+  // see: https://github.com/watson-developer-cloud/node-sdk/blob/master/examples/tone_analyzer.v3.js
+
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
